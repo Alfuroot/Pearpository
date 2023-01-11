da64ea02d550cd38ebe0bbe6cf7c7366f55b88ce
Alfuroot <giuseppe.carannante4@studenti.unina.it>
2023-01-11 16:10:25 +0100

    Starting Project
    Created repo

---
+++ MySmartRestaurant/MySmartRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySmartRestaurantApp.swift
+//  MySmartRestaurant
+//
+//  Created by Giuseppe Carannante on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MySmartRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
