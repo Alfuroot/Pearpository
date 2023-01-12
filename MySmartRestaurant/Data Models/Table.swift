0ccc23682ea476fcb10e969b00b65911d208e45f
Alfuroot <giuseppe.carannante4@studenti.unina.it>
2023-01-12 18:37:56 +0100

    pushing datamodels

---
+++ MySmartRestaurant/Data Models/Table.swift
@@ -0,0 +1,34 @@
+//
+//  Table.swift
+//  MySmartRestaurant
+//
+//  Created by Giuseppe Carannante on 11/01/23.
+//
+
+import Foundation
+
+public class Table {
+    
+    var tableName: String?
+    var isReserved: Reservation?
+    
+    init(tableName: String) {
+        self.tableName = tableName
+        self.isReserved = nil
+    }
+    
+    init(tableName: String, isReserved: Reservation) {
+        self.tableName = tableName
+        self.isReserved = isReserved
+    }
+    
+    init(coreDataEntity: CoreTable){
+        self.tableName = coreDataEntity.tableName
+        self.isReserved = coreDataEntity.isReserved as? Reservation
+    }
+    
+    func copyInEntity(coreDataEntity: CoreTable){
+        coreDataEntity.tableName = self.tableName
+        coreDataEntity.isReserved = self.isReserved
+    }
+}
