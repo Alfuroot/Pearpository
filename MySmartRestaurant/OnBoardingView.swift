3e034b36af78e899d46201c31006c70fccc651bd
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-01-18 16:14:13 +0100

    Added the OnBoardingView

---
+++ MySmartRestaurant/OnBoardingView.swift
@@ -0,0 +1,68 @@
+//
+//  OnBoardingView.swift
+//  MySmartRestaurant
+//
+//  Created by Antonio Scognamiglio on 18/01/23.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    @State var numberOfTables: Int = 8
+    @State var numberOfOutoDoorTables: Int = 0
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                Text("Wecolme to my Smart Restaurant")
+                    .font(.largeTitle)
+                          HStack {
+                        Text("How many tables are there?")
+                            .fontWeight(.medium)
+                        Spacer()
+                          }
+                          .padding()
+                          Picker("Indoor tables", selection: $numberOfTables) {
+                        ForEach(8...200, id: \.self) {
+                            Text("\($0)")
+                        }
+                          }
+                        .pickerStyle(.wheel)
+                        .frame(height: geo.size.height * 0.16)
+                HStack {
+                    Text("How many tables are there?")
+                        .fontWeight(.medium)
+                    Spacer()
+                }
+                .padding()
+                Picker("Outdoor tables", selection: $numberOfOutoDoorTables) {
+              ForEach(8...200, id: \.self) {
+                  Text("\($0)")
+              }
+                }
+              .pickerStyle(.wheel)
+              .frame(height: geo.size.height * 0.16)
+                Spacer()
+                Button {
+                    // More actions to come
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(.blue)
+                            .frame(width: geo.size.width * 0.88, height: geo.size.height * 0.08)
+                        Text("Confirm")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+                          
+                          struct OnBoardingView_Previews: PreviewProvider {
+                        static var previews: some View {
+                            OnBoardingView()
+                        }
+                    }
