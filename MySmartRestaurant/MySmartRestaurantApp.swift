c8f8ec76039eec6ec94cd0f9af58eb3e5e616103
Alfuroot <giuseppe.carannante4@studenti.unina.it>
2023-01-19 16:21:22 +0100

    removed package

--- MySmartRestaurant/MySmartRestaurantApp.swift
+++ MySmartRestaurant/MySmartRestaurantApp.swift
@@ -6,17 +6,14 @@
 //
 
 import SwiftUI
-import FMProKit
 
 @main
 struct MySmartRestaurantApp: App {
     @Environment(\.managedObjectContext) var managedObjectContext
-    @StateObject var FMDataAPIController = FMODataAPI("napoli.fm-testing.com", "MySmartRestaurant", "Admin", "admin", .v4)
     
     var body: some Scene {
         WindowGroup {
             FirstView()
-                .environmentObject(FMDataAPIController)
         }
     }
 }
